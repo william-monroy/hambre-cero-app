7aba65d7f22e07e74860dd7cf0fb96f5e3b8cbb3
luisantoniocrag <tricubeart@gmail.com>
2021-07-25 12:20:40 -0500

    Fix: howWorkssection

--- src/components/how_works/HowWorks.jsx
+++ src/components/how_works/HowWorks.jsx
@@ -1,42 +1,17 @@
-import React from 'react'
-import Card from '../card/Card'
-import './HowWorks.css'
-import { Container, Grid } from "@material-ui/core";
-
+import React from "react";
+import Card from "../card/Card";
+import "./HowWorks.css";
 const HowWorks = () => {
-    return (
-        <div className="howWorks">
-            <h1>¿Cómo funciona?</h1>
+  return (
+    <div className="howWorks">
+      <h1>¿Cómo funciona?</h1>
+      <div className="cards-container">
+        <Card num="1" txt="Suscribete a un plan" />
+        <Card num="2" txt="Ingresa nuestra plataforma o integra directamente" />
+        <Card num="3" txt="Sube tu imagen" />
+      </div>
+    </div>
+  );
+};
 
-            <Grid
-                className="how_Works_box__container"
-                container
-                direction="row"
-                justifyContent="space-between"
-                alignItems="center"
-            >
-                <Grid item>
-                    <Card
-                        num="1"
-                        txt="Suscribete a un plan"
-                    />
-                </Grid>
-                <Grid item>
-                    <Card
-                        num="2"
-                        txt="Ingresa nuestra plataforma
-o integra directamente"
-                    />
-                </Grid>
-                <Grid item>
-                    <Card
-                        num="3"
-                        txt="Sube tu imagen"
-                    />
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
-
-export default HowWorks
+export default HowWorks;
