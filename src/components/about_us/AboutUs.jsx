4b3ada91e25c6babdc4f17270348a2836df4c657
William Monroy <A00829796@itesm.mx>
2021-07-25 10:44:49 -0500

    about section

--- src/components/about_us/AboutUs.jsx
+++ src/components/about_us/AboutUs.jsx
@@ -4,7 +4,11 @@ import './AboutUs.css'
 const AboutUs = () => {
     return (
         <div className="aboutUs">
-            <h1>About</h1>
+            <h1>Acerca de</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde fugit consequuntur, natus temporibus error quidem repellat odit minima, quam alias accusantium deleniti necessitatibus delectus soluta! Repellat beatae voluptatum ut eum?</p>
+            <div className="images">
+
+            </div>
         </div>
     )
 }
