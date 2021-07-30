702c9e63f2ea70848164aa4feb457b006af2914c
William Monroy <A00829796@itesm.mx>
2021-07-30 18:46:24 -0500

    Unused

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Dropdown.css";
 import { Link, useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
