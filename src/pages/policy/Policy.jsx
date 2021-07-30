baa0ca1b7925080c4a1983d3abb5f7b9bb37cf20
William Monroy <A00829796@itesm.mx>
2021-07-29 19:53:27 -0500

    Unused Components

--- src/pages/policy/Policy.jsx
+++ src/pages/policy/Policy.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, Card, Typography } from "@material-ui/core";
+import { Container, Grid, Typography } from "@material-ui/core";
 import "./Policy.css"
 
 const terms = [
